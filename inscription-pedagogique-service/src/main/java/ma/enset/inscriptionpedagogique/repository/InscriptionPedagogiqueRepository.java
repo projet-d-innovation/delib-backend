9ef75d8e122a8680d0b479a05bd275d7f6c37cfa
aymane <77883489+oaymn@users.noreply.github.com>
2023-06-04 00:18:35 +0100

    Repository: add the option to delete inscriptions related to a student

--- inscription-pedagogique-service/src/main/java/ma/enset/inscriptionpedagogique/repository/InscriptionPedagogiqueRepository.java
+++ inscription-pedagogique-service/src/main/java/ma/enset/inscriptionpedagogique/repository/InscriptionPedagogiqueRepository.java
@@ -3,15 +3,19 @@ package ma.enset.inscriptionpedagogique.repository;
 import ma.enset.inscriptionpedagogique.model.InscriptionPedagogique;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface InscriptionPedagogiqueRepository extends JpaRepository<InscriptionPedagogique, Long> {
     List<InscriptionPedagogique> findAllByCodeFiliereAndCodeSessionUniversitaireAndAnnee(
-        String codeFilier, String codeSessionUniversitaire, int anne
+        String codeFilier, String codeSessionUniversitaire, int annee
     );
 
     List<InscriptionPedagogique> findAllByCodeEtudiant(String codeEtudiant);
+    
+    @Transactional
+    void deleteAllByCodeEtudiant(String codeEtudiant);
 
 }
