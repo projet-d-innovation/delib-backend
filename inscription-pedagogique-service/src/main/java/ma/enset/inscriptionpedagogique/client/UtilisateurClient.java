c91af2351f2431ee27cfbf8700e1b85c1f9d2d66
aymane <77883489+oaymn@users.noreply.github.com>
2023-06-05 00:43:54 +0100

    UtilisateurClient: add a method to retrieve a single student by id

--- inscription-pedagogique-service/src/main/java/ma/enset/inscriptionpedagogique/client/UtilisateurClient.java
+++ inscription-pedagogique-service/src/main/java/ma/enset/inscriptionpedagogique/client/UtilisateurClient.java
@@ -2,6 +2,7 @@ package ma.enset.inscriptionpedagogique.client;
 
 import ma.enset.inscriptionpedagogique.dto.EtudiantResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -14,6 +15,9 @@ public interface UtilisateurClient {
 
     @GetExchange(url = "/exists")
     void allEtudiantsExist(@RequestParam("codes") Set<String> codes);
+
+    @GetExchange(url = "/{code}")
+    ResponseEntity<EtudiantResponse> getEtudiantById(@PathVariable("code") String code);
 
     @GetExchange(url = "/bulk")
     ResponseEntity<List<EtudiantResponse>> getAllEtudiantsByIds(@RequestParam("codes") Set<String> codes);
