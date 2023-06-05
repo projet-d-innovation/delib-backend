3d95675b9d9704fa52db3672ea8b1e4b29bf73ea
Elbouchouki <elbouchoukigamer@gmail.com>
2023-06-05 18:14:34 +0100

    add delib to gateway

--- deliberation-service/src/main/java/ma/enset/deliberationservice/DeliberationServiceApplication.java
+++ deliberation-service/src/main/java/ma/enset/deliberationservice/DeliberationServiceApplication.java
@@ -2,12 +2,12 @@ package ma.enset.deliberationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
+@EnableDiscoveryClient
 public class DeliberationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DeliberationServiceApplication.class, args);
     }
--- gateway-server/src/main/java/ma/enset/gateway/GatewayApplication.java
+++ gateway-server/src/main/java/ma/enset/gateway/GatewayApplication.java
@@ -79,6 +79,13 @@ public class GatewayApplication {
                                 .filters(f -> f.rewritePath("/semestre-service/docs", "/v3/api-docs"))
                                 .uri("lb://semestre-service")
                 )
+                .route(
+                        r -> r.path("/api/v1/session/**")
+                                .or()
+                                .path("/deliberation-service/docs")
+                                .filters(f -> f.rewritePath("/deliberation-service/docs", "/v3/api-docs"))
+                                .uri("lb://deliberation-service")
+                )
                 .build();
     }
 }
