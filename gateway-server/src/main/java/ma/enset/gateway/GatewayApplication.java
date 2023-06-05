f63337148ac7d942546337e827a3255ef2eec3d4
aymane <77883489+oaymn@users.noreply.github.com>
2023-06-05 20:40:41 +0100

    Add `Inscription pedagogique service` entry to the gateway

--- gateway-server/src/main/java/ma/enset/gateway/GatewayApplication.java
+++ gateway-server/src/main/java/ma/enset/gateway/GatewayApplication.java
@@ -1,12 +1,9 @@
 package ma.enset.gateway;
 
 
-import com.netflix.discovery.DiscoveryClient;
-import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -22,77 +19,86 @@ public class GatewayApplication {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(
-                        r -> r.path("/eureka/web")
-                                .filters(f -> f.setPath("/"))
-                                .uri("http://localhost:8761")
-                )
-                .route(
-                        r -> r.path("/eureka/**")
-                                .uri("http://localhost:8761")
-                )
-                .route(
-                        r -> r.path("/api/v1/utilisateurs/**")
-                                .or()
-                                .path("/api/v1/roles/**")
-                                .or()
-                                .path("/api/v1/permissions/**")
-                                .or()
-                                .path("/utilisateur-service/docs")
-                                .filters(f -> f.rewritePath("/utilisateur-service/docs", "/v3/api-docs"))
-                                .uri("lb://utilisateur-service")
-                )
-                .route(
-                        r -> r.path("/api/v1/elements/**")
-                                .or()
-                                .path("/element-service/docs")
-                                .filters(f -> f.rewritePath("/element-service/docs", "/v3/api-docs"))
-                                .uri("lb://element-service")
-                )
-                .route(
-                        r -> r.path("/api/v1/modules/**")
-                                .or()
-                                .path("/module-service/docs")
-                                .filters(f -> f.rewritePath("/module-service/docs", "/v3/api-docs"))
-                                .uri("lb://module-service")
-                )
-                .route(
-                        r -> r.path("/api/v1/departements/**")
-                                .or()
-                                .path("/departement-service/docs")
-                                .filters(f -> f.rewritePath("/departement-service/docs", "/v3/api-docs"))
-                                .uri("lb://departement-service")
-                )
-                .route(
-                        r -> r.path("/api/v1/filieres/**")
-                                .and()
-                                .path("/api/v1/regledecalcul/**")
-                                .or()
-                                .path("/filiere-service/docs")
-                                .filters(f -> f.rewritePath("/filiere-service/docs", "/v3/api-docs"))
-                                .uri("lb://filiere-service")
-                )
-                .route(
-                        r -> r.path("/api/v1/semestres/**")
-                                .or()
-                                .path("/semestre-service/docs")
-                                .filters(f -> f.rewritePath("/semestre-service/docs", "/v3/api-docs"))
-                                .uri("lb://semestre-service")
-                )
-                .route(
-                        r -> r.path("/api/v1/session/**")
-                                .or()
-                                .path("/deliberation-service/docs")
-                                .filters(f -> f.rewritePath("/deliberation-service/docs", "/v3/api-docs"))
-                                .uri("lb://deliberation-service")
-                )
-               .route(
-                        r -> r.path("/api/v1/notes/**")
-                                .or()
-                                .path("/note-service/docs")
-                                .filters(f -> f.rewritePath("/note-service/docs", "/v3/api-docs"))
-                                .uri("lb://note-service")
-                )
-                .build();
+            .route(
+                r -> r.path("/eureka/web")
+                    .filters(f -> f.setPath("/"))
+                    .uri("http://localhost:8761")
+            )
+            .route(
+                r -> r.path("/eureka/**")
+                    .uri("http://localhost:8761")
+            )
+            .route(
+                r -> r.path("/api/v1/utilisateurs/**")
+                    .or()
+                    .path("/api/v1/roles/**")
+                    .or()
+                    .path("/api/v1/permissions/**")
+                    .or()
+                    .path("/utilisateur-service/docs")
+                    .filters(f -> f.rewritePath("/utilisateur-service/docs", "/v3/api-docs"))
+                    .uri("lb://utilisateur-service")
+            )
+            .route(
+                r -> r.path("/api/v1/elements/**")
+                    .or()
+                    .path("/element-service/docs")
+                    .filters(f -> f.rewritePath("/element-service/docs", "/v3/api-docs"))
+                    .uri("lb://element-service")
+            )
+            .route(
+                r -> r.path("/api/v1/modules/**")
+                    .or()
+                    .path("/module-service/docs")
+                    .filters(f -> f.rewritePath("/module-service/docs", "/v3/api-docs"))
+                    .uri("lb://module-service")
+            )
+            .route(
+                r -> r.path("/api/v1/departements/**")
+                    .or()
+                    .path("/departement-service/docs")
+                    .filters(f -> f.rewritePath("/departement-service/docs", "/v3/api-docs"))
+                    .uri("lb://departement-service")
+            )
+            .route(
+                r -> r.path("/api/v1/filieres/**")
+                    .and()
+                    .path("/api/v1/regledecalcul/**")
+                    .or()
+                    .path("/filiere-service/docs")
+                    .filters(f -> f.rewritePath("/filiere-service/docs", "/v3/api-docs"))
+                    .uri("lb://filiere-service")
+            )
+            .route(
+                r -> r.path("/api/v1/semestres/**")
+                    .or()
+                    .path("/semestre-service/docs")
+                    .filters(f -> f.rewritePath("/semestre-service/docs", "/v3/api-docs"))
+                    .uri("lb://semestre-service")
+            )
+            .route(
+                r -> r.path("/api/v1/session/**")
+                    .or()
+                    .path("/deliberation-service/docs")
+                    .filters(f -> f.rewritePath("/deliberation-service/docs", "/v3/api-docs"))
+                    .uri("lb://deliberation-service")
+            )
+            .route(
+                r -> r.path("/api/v1/notes/**")
+                    .or()
+                    .path("/note-service/docs")
+                    .filters(f -> f.rewritePath("/note-service/docs", "/v3/api-docs"))
+                    .uri("lb://note-service")
+            )
+            .route(
+                r -> r.path("/api/v1/inscriptions-pedagogique/**")
+                    .and()
+                    .path("/api/v1/etudiants/**")
+                    .or()
+                    .path("/inscription-pedagogique-service/docs")
+                    .filters(f -> f.rewritePath("/inscription-pedagogique-service/docs", "/v3/api-docs"))
+                    .uri("lb://inscription-pedagogique-service")
+            )
+            .build();
     }
 }
